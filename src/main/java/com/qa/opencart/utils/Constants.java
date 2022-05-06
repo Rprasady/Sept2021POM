940188b00b8c341b969d92f04bd635fdb7fc8c51
Raviprasad Yarapu <v-ryarapu@microsoft.com>
2022-05-06 13:55:57 +0530

    added cart page

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public class Constants {
 	
-	public static final String LOGIN_PAGE_TITLE = "Account Login1";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String LOGIN_PAGE_URL_FRACTION = "https://demo.opencart.com/index.php?route=account/login" ;
 	public static final int DEFAULT_TIME_OUT = 5;
 	public static final String ACCOUNT_PAGE_TITLE = "My Account";
-	public static final String ACCOUNT_PAGE_HEADER = "Your Store1";
+	public static final String ACCOUNT_PAGE_HEADER = "Your Store";
 	public static final int MACBOOKPRO_IMAGE_COUNT = 4;
 	public static final String LOGIN_ERROR_MESSAGE = "No match for E-Mail Address and/or Password.";
 	public static final String REGISTER_SUCCESS_MESSAGE = "Your Account Has Been Created!";
